2ddca857c9f1fd7ccc7d59db94ade43231544a58
Yu-Jen Lin <yjlin5210@gmail.com>
2017-03-13 23:50:56 -0400

    update

--- Assets/Scripts/TrackerHub.cs
+++ Assets/Scripts/TrackerHub.cs
@@ -9,17 +9,25 @@ public class TrackerHub : MonoBehaviour {
 	public Canvas canvas;
 	public FingerUI fingerPointPrefab;
 	public MarkerUI markerPrefab;
+	public MarkerGO markerGoPrefab;
 	private Dictionary<int, FingerUI> fingerPoints;
 	private Dictionary<int, MarkerUI> markerPoints;
-	float lastUpdateTime;
-	float CLEARTIME = 0.5f;
+
+	private Dictionary<int, MarkerGO> markerGOs;
+
+	public MarkerGO activeMarker;
+
+	private bool waitSpeech = false;
+	// float lastUpdateTime;
+	const float CLEARTIME = 0.5f;
 
 	
 	// Use this for initialization
 	void Start () {
 		fingerPoints = new Dictionary<int, FingerUI>();
 		markerPoints = new Dictionary<int, MarkerUI>();
-		lastUpdateTime = Time.time;
+		markerGOs = new Dictionary<int, MarkerGO>();
+		// lastUpdateTime = Time.time;
 	}
 	
 	// Update is called once per frame
@@ -31,27 +39,28 @@ public class TrackerHub : MonoBehaviour {
 			if (jo["fingers"] != null) {
 				updateFingers((JSONClass)jo["fingers"]);
 			} else {
-				clearAllFingers();
+				// clearAllFingers();
 			}
 
 			if (jo["markers"] != null) {
-				updateMarkers((JSONArray)jo["markers"]);
+				updateMarkers((JSONClass)jo["markers"]);
 			} else {
-				clearAllMarkers();
+				// clearAllMarkers();
 			}
-			lastUpdateTime = Time.time;
+			// lastUpdateTime = Time.time;
 		}
 
-		if (Time.time - lastUpdateTime > CLEARTIME) {
-			clearAllMarkers();
-			clearAllFingers();
+		if (waitSpeech) {
+			Camera.main.backgroundColor = new Color32(200, 100, 100, 255);
+		} else {
+			Camera.main.backgroundColor = new Color32(49, 77, 121, 255);
 		}
 	}
 	void updateFingers(JSONClass fingers) {
 		foreach (string k in fingers.keys) {
 			int id = fingers[k]["id"].AsInt;
 			float x = fingers[k]["x"].AsFloat;
-			float y = 1080 - fingers[k]["y"].AsFloat;
+			float y = 900 - fingers[k]["y"].AsFloat;
 			int action = fingers[k]["action"].AsInt;
 			// Debug.Log( "Finger " + id + ",  Action: " + action);
 			Finger finger = new Finger(id, x, y);
@@ -59,18 +68,6 @@ public class TrackerHub : MonoBehaviour {
 			RectTransform t;
 			switch (action) {
 				case 1:
-					if (fingerPoints.ContainsKey(id)) {
-						fingerUI = fingerPoints[id];
-					} else {
-						fingerUI = GameObject.Instantiate<FingerUI>(fingerPointPrefab);
-						fingerUI.transform.SetParent(canvas.transform, false);
-						fingerUI.SetFinger(finger);
-						fingerPoints[id] = fingerUI;
-					}
-					t = fingerUI.GetComponent<RectTransform>();
-					t.anchoredPosition = new Vector2(x, y);
-					fingerPoints[id] = fingerUI;
-					break;
 				case 2:
 					if (fingerPoints.ContainsKey(id)) {
 						fingerUI = fingerPoints[id];
@@ -86,6 +83,7 @@ public class TrackerHub : MonoBehaviour {
 				case 3:
 					if (fingerPoints.ContainsKey(id)) {
 						fingerUI = fingerPoints[id];
+						fingerUI.gameObject.SetActive(false);
 						GameObject.DestroyImmediate(fingerUI.gameObject);
 						fingerPoints.Remove(id);
 					}
@@ -97,47 +95,63 @@ public class TrackerHub : MonoBehaviour {
 		foreach(var key in keys) {
 			if (fingers[key.ToString()] == null) {
 				FingerUI fingerUI = fingerPoints[key];
+				fingerUI.gameObject.SetActive(false);
 				GameObject.DestroyImmediate(fingerUI.gameObject);
 				fingerPoints.Remove(key);
 			}
 		}
 	}
-	void updateMarkers(JSONArray markers) {
-		for (int i = 0; i < markers.Count; i++) {
+
+	void updateMarkers(JSONClass markers) {
+		foreach (string k in markers.keys) {
 			// Debug.Log("Marker " + markers[i]["id"].AsInt);
-			int id = markers[i]["id"].AsInt;
-			float x = markers[i]["center"]["x"].AsFloat;
-			float y = 1080 - markers[i]["center"]["y"].AsFloat;
-			float angle = markers[i]["angle"].AsFloat;
+			int id = markers[k]["id"].AsInt;
+			float x = markers[k]["center"]["x"].AsFloat;
+			float y = 900 - markers[k]["center"]["y"].AsFloat;
+			float angle = markers[k]["angle"].AsFloat;
+			int action = markers[k]["action"].AsInt;
 			Marker marker = new Marker(id, x, y, angle);
 			MarkerUI markerUI;
-			if (markerPoints.ContainsKey(id)) {
-				markerUI = markerPoints[id];
-			} else {
-				markerUI = GameObject.Instantiate<MarkerUI>(markerPrefab);
-				markerUI.transform.SetParent(canvas.transform, false);
-				markerUI.SetMarker(marker);
-				markerPoints[id] = markerUI;
-			}
-			RectTransform t = markerUI.GetComponent<RectTransform>();
-			t.anchoredPosition = new Vector2(x, y);
-			t.eulerAngles = new Vector3(0, 0, angle);
-		}
-
-		List<int> keys = new List<int>(markerPoints.Keys);	
-		foreach(var key in keys) {
-			bool found = false;
-			for (int i = 0; i < markers.Count; i++) {
-				int id = markers[i]["id"].AsInt;
-				if (id == key) {
-					found = true;
-					continue;
-				}
-			}
-			if (!found) {
-				MarkerUI markerUI = markerPoints[key];
-				GameObject.DestroyImmediate(markerUI.gameObject);
-				markerPoints.Remove(key);
+			MarkerGO markerGO;
+			RectTransform t;
+			Transform trans;
+			switch (action) {
+				case 1:
+				case 2:
+					if (markerPoints.ContainsKey(id)) {
+						markerUI = markerPoints[id];
+						markerGO = markerGOs[id];
+						markerGO.setActive(true);
+					} else {
+						markerUI = GameObject.Instantiate<MarkerUI>(markerPrefab);
+						markerGO = GameObject.Instantiate<MarkerGO>(markerGoPrefab);
+						markerUI.transform.SetParent(canvas.transform, false);
+						markerGOs[id] = markerGO;
+						markerUI.SetMarker(marker);
+						markerGO.marker = marker;
+						markerPoints[id] = markerUI;
+					}
+					t = markerUI.GetComponent<RectTransform>();
+					t.anchoredPosition = new Vector2(x, y);
+					t.eulerAngles = new Vector3(0, 0, angle);
+					trans = markerGO.transform;
+					Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 10));
+					trans.position = pos;
+					trans.eulerAngles = new Vector3(0, 0, angle);
+					break;
+				case 3:
+					if (markerPoints.ContainsKey(id)) {
+						markerUI = markerPoints[id];
+						markerGO = markerGOs[id];
+						markerUI.gameObject.SetActive(false);
+						markerGO.setActive(false);
+						// GameObject.DestroyImmediate(markerUI.gameObject);
+						// GameObject.DestroyImmediate(markerGO.gameObject);
+						// markerPoints.Remove(id);
+						// markerGOs.Remove(id);
+					}
+					break;
+				
 			}
 		}
 	}
@@ -147,9 +161,12 @@ public class TrackerHub : MonoBehaviour {
 		foreach(var key in keys) {
 			MarkerUI markerUI = markerPoints[key];
 			GameObject.DestroyImmediate(markerUI.gameObject);
+			MarkerGO markerGO = markerGOs[key];
+			GameObject.DestroyImmediate(markerGO.gameObject);
 		}
 
 		markerPoints.Clear();
+		markerGOs.Clear();
 	}
 
 	void clearAllFingers() {
@@ -160,6 +177,31 @@ public class TrackerHub : MonoBehaviour {
 		}
 
 		fingerPoints.Clear();
+	}
+
+	public void setActiveMarker(int id) {
+		MarkerGO go = markerGOs[id];
+		if (go != null) {
+			activeMarker = go;
+			activeMarker.setHighlight();
+		}
+		// Renderer r = activeMarker.GetComponent<Renderer>();
+		// r.material.color = new Color32(200, 200, 0, 255);
+	}
+
+	public void removeActiveMarker() {
+		if (activeMarker) {
+			activeMarker.unsetHighlight();
+			activeMarker = null;
+		}
+	}
+
+	public void setHotword() {
+		waitSpeech = true;
+	}
+
+	public void unsetHotword() {
+		waitSpeech = false;
 	}
 }
 
---
+++ Assets/Scripts/Trackers/GameObject/MarkerGO.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class MarkerGO : MonoBehaviour {
+	public AudioClip[] soundclips;
+
+	private Dictionary<string, int> audioMap = new Dictionary<string, int> {
+		{"Lion", 0},
+		{"Frog", 1},
+		{"Monkey", 2},
+		{"Wolf", 3},
+		{"Rainforest", 4},
+		{"Elephant", 5}
+	};
+
+	private int audioIndex = -1;
+	private bool isEnabled = true;
+
+	private AudioSource asource;
+	private Renderer renderer;
+	public bool highlight;
+	private bool setAudioFlag = false;
+	public Marker marker {
+		set; get;
+	}
+
+	void Start() {
+		asource = gameObject.GetComponent<AudioSource>();
+		renderer = gameObject.GetComponent<Renderer>();
+	}
+
+	void Update() {
+		if (!isEnabled) {
+			return;
+		}
+		if (highlight) {
+			renderer.material.color = new Color32(200, 200, 0, 255);
+			asource.volume = 0.2f;
+			if (setAudioFlag) {
+				asource.clip = soundclips[audioIndex];
+				if (!asource.isPlaying) {
+					asource.Play();
+				}
+				setAudioFlag = false;
+			}
+		} else {
+			if (asource.clip != null) {
+				asource.volume = 1.0f;
+				if (!asource.isPlaying) {
+					asource.Play();
+				}
+				renderer.material.color = new Color32(0, 100, 200, 255);
+			} else {
+				renderer.material.color = new Color32(255, 255, 255, 255);
+				asource.Pause();
+			}
+		}
+	}
+
+	public void setHighlight() {
+		highlight = true;
+	}
+
+	public void unsetHighlight() {
+		highlight = false;
+	}
+
+	public void setAudio(string input) {
+		audioIndex = audioMap[input];
+		setAudioFlag = true; 
+	}
+
+	public void setActive(bool value) {
+		if (value) {
+			isEnabled = true;
+			if (asource.clip && !asource.isPlaying) {
+				asource.Play();
+			}
+			renderer.enabled = true;
+		} else {
+			isEnabled = false;
+			asource.Pause();
+			renderer.enabled = false;
+		}
+	}
+}
